1de1b1bd2e8d952dc6ccbcf574830a3799c86ecb
444ten <444ten@gmail.com>
2015-02-12 16:44:04 +0200

    struct

---
+++ 0211 struct/Structure/Structure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Structure
+//
+//  Created by 444ten on 2/12/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#include "TENStructTest.h"
+
+int main(int argc, const char * argv[]) {
+    TENStructPerformTest();
+    
+    return 0;
+}
