ddfbce88d1f1e9811f64ab52315cad2e0b93bad7
444ten <444ten@gmail.com>
2015-03-26 13:20:10 +0200

    TENNode -> initial commit

---
+++ HomeworkC/HomeworkC/TENNode/TENNode.h
@@ -0,0 +1,14 @@
+//
+//  TENNode.h
+//  HomeworkC
+//
+//  Created by 444ten on 3/26/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#ifndef __HomeworkC__TENNode__
+#define __HomeworkC__TENNode__
+
+#include <stdio.h>
+
+#endif /* defined(__HomeworkC__TENNode__) */
