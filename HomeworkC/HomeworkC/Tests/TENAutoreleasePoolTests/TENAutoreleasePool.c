8ab2126e14d43ad0a884f1379dd6ffac07b51e0a
444ten <444ten@gmail.com>
2015-04-06 12:06:21 +0300

    TENAutoreleasePoolTests - initial commit

---
+++ HomeworkC/HomeworkC/Tests/TENAutoreleasePoolTests/TENAutoreleasePool.c
@@ -0,0 +1,23 @@
+//
+//  TENAutoreleasePool.c
+//  HomeworkC
+//
+//  Created by 444ten on 4/6/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#include "TENAutoreleasePool.h"
+
+#pragma mark -
+#pragma mark Private Declarations
+
+
+#pragma mark -
+#pragma mark Public Implementations
+
+void TENAutoreleasePoolPerformTests() {
+    
+}
+
+#pragma mark -
+#pragma mark Private Implementations
