23d5fcba2aaeb60ff71f467da82ed8e0aa7a757b
444ten <444ten@gmail.com>
2015-04-06 12:24:12 +0300

    TENAutoreleasePool -> initial commit

---
+++ HomeworkC/HomeworkC/TENAutoreleasePool/TENAutoreleasePool.h
@@ -0,0 +1,14 @@
+//
+//  TENAutoreleasePool.h
+//  HomeworkC
+//
+//  Created by 444ten on 4/6/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#ifndef __HomeworkC__TENAutoreleasePool__
+#define __HomeworkC__TENAutoreleasePool__
+
+#include <stdio.h>
+
+#endif /* defined(__HomeworkC__TENAutoreleasePool__) */
